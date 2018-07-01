2fe571d588b565623151a03a376dc0d57492e117
jakelheknight <jakelheknight@gmail.com>
2018-07-01 00:42:41 -0600

    Getting close

--- gochatwin.go
+++ gochatwin.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"time"
 )
@@ -22,104 +23,124 @@ func (message msg) format(channel string) string {
 type channel struct {
 	name      string
 	msgStream chan msg
-	users     map[string]user // Key is to match user.name
+}
+
+func (channel channel) systemMsg(message string) msg {
+	return msg{
+		text:      message,
+		sender:    "System",
+		timeStamp: time.Now(),
+	}
 }
 
 type user struct {
-	name    string
-	focused channel
+	name       string
+	focused    channel
+	subscribed map[string]channel
 }
 
-// A few maps of objects registered with the server. When you set a server up it must have a GENERAL channel added to send system level messages.
-type server struct {
+// A few maps of objects registered with the server. When you set a chatManager up it must have a GENERAL channel added to send system level messages.
+type chatManager struct {
 	users       map[string]user
 	channelList map[string]channel
 }
 
-func (server *server) makeChannel(channelName string) {
-	if _, ok := server.channelList[channelName]; !ok {
-		server.channelList[channelName] = channel{
+func (chatManager *chatManager) makeChannel(channelName string) {
+	if _, ok := chatManager.channelList[channelName]; !ok {
+		chatManager.channelList[channelName] = channel{
 			name:      channelName,
 			msgStream: make(chan msg, 0),
-			users:     make(map[string]user, 0),
 		}
-		server.channelList["GENERAL"].msgStream <- msg{
-			text:      fmt.Sprintf("New Channel: %s is ready for use.", channelName),
-			sender:    "System",
-			timeStamp: time.Now(),
-		}
+		chatManager.channelList["GENERAL"].msgStream <- chatManager.channelList[channelName].systemMsg(fmt.Sprintf("New Channel: %s is ready for use.", channelName))
 	} else {
-		server.channelList["GENERAL"].msgStream <- msg{
-			text:      fmt.Sprintf("Channel: %s already exists.", channelName),
-			sender:    "System",
-			timeStamp: time.Now(),
-		}
+		chatManager.channelList["GENERAL"].msgStream <- chatManager.channelList[channelName].systemMsg(fmt.Sprintf("Channel: %s already exists.", channelName))
 	}
 }
 
-func (server *server) joinChannel(userName string, channelName string) {
-	if _, ok := server.channelList[channelName]; ok {
-		joinUser := server.users[userName]
-		joinUser.focused = server.channelList[channelName]
-		if _, ok := server.users[userName]; !ok {
-			server.channelList[channelName].users[userName] = joinUser
-		}
-		// reassigning this user gets aroudn an issue with grabbing an non addressable item inside a map. There are two solutions coppying the item or using pointers.
-		server.users[userName] = joinUser
+func (chatManager *chatManager) joinChannel(userName string, channelName string) {
+	if _, ok := chatManager.channelList[channelName]; !ok {
+		chatManager.makeChannel(channelName)
 	}
+	joinUser := chatManager.users[userName]
+	joinUser.focused = chatManager.channelList[channelName]
+	if _, ok := chatManager.users[userName].subscribed[channelName]; !ok {
+		chatManager.users[userName].subscribed[channelName] = chatManager.channelList[channelName]
+	}
+	// reassigning this user gets aroudn an issue with grabbing an non addressable item inside a map. There are two solutions coppying the item or using pointers.
+	chatManager.users[userName] = joinUser
 
+	chatManager.channelList[channelName].msgStream <- chatManager.channelList[channelName].systemMsg(fmt.Sprintf("%s has joint the channel. Say hello.", userName))
 }
 
 var general = channel{
 	name:      "General",
 	msgStream: make(chan msg),
-	users:     make(map[string]user, 0),
 }
 
-func handleUserConnection(server *server, conn net.Conn) {
+func handleUserConnection(chatManager *chatManager, conn net.Conn) {
 	defer conn.Close()
+
 	scanner := bufio.NewScanner(conn)
-	var currentUser user
-	var welcomeMsg = msg{
-		text:      "Welcome to GoChatWin an awesome chat server pleas chose a UserName: ",
-		sender:    "System",
-		timeStamp: time.Now(),
-	}
-	var userSucessMsg = msg{
-		text:      "Thanks for joining us. Type /help for a list of commands. ",
-		sender:    "System",
-		timeStamp: time.Now(),
-	}
-	var userFailMsg = msg{
-		text:      "Sorry that user name is taken Please choose another one:",
-		sender:    "System",
-		timeStamp: time.Now(),
-	}
-	io.WriteString(conn, welcomeMsg.format("GENERAL"))
+	var userName string
+	io.WriteString(conn, chatManager.channelList["GENERAL"].systemMsg("Welcome to GoChatWin an awesome chat server pleas chose a UserName: ").format("GENERAL"))
 
 	// Loop so that users will be unique and not overwritte eachother.
-	// Could also pull a bungy and postpend a random string of chars at the end but I like this better even if you could use invisable chars.
+	// Could also postpend a random string of chars at the end but I like this better even if you could use invisable chars this way you could add wisper if you wanted.
 	for {
 		scanner.Scan()
-		userName := scanner.Text()
-		if _, ok := server.users[userName]; ok {
-			currentUser = user{
+		userName = scanner.Text()
+		if _, ok := chatManager.users[userName]; ok {
+
+			chatManager.users[userName] = user{
 				name:    userName,
 				focused: general,
 			}
-			server.users[userName] = currentUser
 
-			io.WriteString(conn, userSucessMsg.format("GENERAL"))
+			io.WriteString(conn, chatManager.channelList["GENERAL"].systemMsg("Thanks for joining us. Type /help for a list of commands. ").format("GENERAL"))
 
 			break
 		}
-		io.WriteString(conn, userFailMsg.format("GENERAL"))
+		io.WriteString(conn, chatManager.channelList["GENERAL"].systemMsg("Sorry that user name is taken Please choose another one:").format("GENERAL"))
 	}
 
-	server.joinChannel(currentUser.name, "GENERAL")
+	chatManager.joinChannel(userName, "GENERAL")
 
+	go func() {
+		for scanner.Scan() {
+			chatManager.users[userName].focused.msgStream <- msg{
+				text:      scanner.Text(),
+				sender:    chatManager.users[userName].name,
+				timeStamp: time.Now(),
+			}
+		}
+	}()
+
+	for _, channel := range chatManager.users[userName].subscribed {
+		for message := range channel.msgStream {
+			io.WriteString(conn, message.format(channel.name))
+		}
+	}
 }
 
 func main() {
+	server, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer server.Close()
 
+	chatManager := &chatManager{
+		users:       make(map[string]user, 0),
+		channelList: make(map[string]channel, 0),
+	}
+
+	chatManager.makeChannel("GENERAL")
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		go handleUserConnection(chatManager, conn)
+	}
 }
